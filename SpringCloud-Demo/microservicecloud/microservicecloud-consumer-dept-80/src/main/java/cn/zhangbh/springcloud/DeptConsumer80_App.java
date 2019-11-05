f15e083172551283db03681292108a3e0bbd2acd
zhangbohua <37918621+zhangbohua@users.noreply.github.com>
2019-11-05 22:13:07 +0800

    springcloud基础逻辑架构搭建

---
+++ SpringCloud-Demo/microservicecloud/microservicecloud-consumer-dept-80/src/main/java/cn/zhangbh/springcloud/DeptConsumer80_App.java
@@ -0,0 +1,11 @@
+package cn.zhangbh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptConsumer80_App {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer80_App.class, args);
+    }
+}
